c979385859beb32cb983a19ffec452be8b0bc2dd
bullitarros <robert@robert-SATELLITE-C855-10W.(none)>
2013-08-12 22:52:39 +0200

    update

---
+++ dibuixa_bs/c_color_codes.js
@@ -0,0 +1,14 @@
+var colorCodes = new Array;
+colorCodes.push("#FF0000");
+colorCodes.push("#FF7400");
+colorCodes.push("#FFAA00");
+colorCodes.push("#FFD300");
+colorCodes.push("#FFFF00");
+colorCodes.push("#9FEE00");
+colorCodes.push("#00CC00");
+colorCodes.push("#009999");
+colorCodes.push("#1240AB");
+colorCodes.push("#3914AF");
+colorCodes.push("#7109AA");
+colorCodes.push("#CD0074");
+
