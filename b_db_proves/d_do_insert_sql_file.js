17fe1d93a08aa6b593cd403ff6c534ea1757f3e1
robert <robert@rob-ubu-10.(none)>
2013-09-03 17:59:59 +0200

    sync

---
+++ b_db_proves/d_do_insert_sql_file.js
@@ -0,0 +1,40 @@
+var fs = require('fs');
+
+var bs = {
+	//BS_key	automatic field!
+	
+	company_id: 0,			
+	unofficial_name: "RONSA",							
+	year: 0,									
+	period: "ANUAL",								
+	
+	A_activo_no_corriente: 0,				
+	I_inmovilizado_intangible: 0,				
+	II_inmovilizado_material: 0,				
+	III_inversiones_inmobiliarias: 0,			
+	IV_inversiones_en_empresas_del_grupo: 0, 	
+	V_inversiones_financieras_a_largo: 0,		
+	VI_activos_por_impuesto_diferido: 0,		
+	VII_otros_activos_no_corrientes: 0,			
+
+	B_activo_corriente: 0,						
+	I_activos_no_corrientes_para_la_venta: 0,	
+	II_existencias: 0,							
+	III_deudores_comerciales: 0,				
+	IV_inversiones_en_empresas_del_grupo_a_corto: 0,	
+	V_inversiones_financieras_a_corto: 0,		
+	VI_periodificaciones_a_corto: 0,			
+	VII_efectivo: 0,							
+	
+	total_activo: 0,							
+	
+	
+	A_patrimonio_neto: 0,						
+	B_pasivo_no_corriente: 0,					
+	C_pasivo_corriente: 0,						
+	
+	total_patrimonio_neto_y_pasivo: 0				
+
+};
+
+for
